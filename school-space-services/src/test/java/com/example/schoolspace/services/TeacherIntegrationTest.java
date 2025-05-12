5d812a7dfa1df2351f2f768dd955813e9622d153
joelpangop <joelpangopgmail.com>
2025-05-12 19:39:32 -0400

    Fix H2 config for integration tests3

--- school-space-services/src/test/java/com/example/schoolspace/services/TeacherIntegrationTest.java
+++ school-space-services/src/test/java/com/example/schoolspace/services/TeacherIntegrationTest.java
@@ -23,9 +23,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-@EntityScan(basePackages = "com.example.schoolspace.model")
-@EnableJpaRepositories(basePackages = "com.example.schoolspace.repository")
-@ComponentScan(basePackages = "com.example.schoolspace")
 public class TeacherIntegrationTest {
 
     @Autowired
