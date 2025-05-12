a3677f152a1cbda9884cc800a101dd370170fe9e
joelpangop <joelpangopgmail.com>
2025-05-12 19:58:19 -0400

    Fix H2 config for integration tests4

--- school-space-services/src/test/java/com/example/schoolspace/services/TeacherIntegrationTest.java
+++ school-space-services/src/test/java/com/example/schoolspace/services/TeacherIntegrationTest.java
@@ -1,23 +1,18 @@
 package com.example.schoolspace.services;
 
 import com.example.schoolspace.dto.TeacherDto;
-import com.example.schoolspace.model.Teacher;
 import com.example.schoolspace.repository.TeacherRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
