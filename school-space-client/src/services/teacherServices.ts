13c3353a325e1dfa5515d43421a40752fca99ef6
joelpangop <joelpangopgmail.com>
2025-05-19 13:14:34 -0400

    add cors for rest services 2

--- school-space-client/src/services/teacherServices.ts
+++ school-space-client/src/services/teacherServices.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import {Teacher} from "../models/Teacher";
 
 const rest_path = 'api/teacher';
-const BASE_URL = `${process.env.REACT_APP_API_URL}/${rest_path}` || `http://localhost:8080/${rest_path}` || `http://http://34.238.242.234/:8080/${rest_path}`;
+const BASE_URL = `${process.env.REACT_APP_API_URL}/${rest_path}` || `http://localhost:8080/${rest_path}` || `http://34.238.242.234:8080/${rest_path}`;
 
 export const getTeachers = async (): Promise<Teacher[]> => {
     const response = await axios.get(`${BASE_URL}`);
