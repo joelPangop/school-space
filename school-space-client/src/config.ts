bbd015218b7b50b5474ae68cb01422bcf02109c4
joelpangop <joelpangopgmail.com>
2025-05-27 14:24:21 -0400

    adapt backend url 12

--- school-space-client/src/config.ts
+++ school-space-client/src/config.ts
@@ -1,4 +1,5 @@
 // @ts-ignore
 // const API_BASE_URL = `http://54.165.49.100:3000/${rest_path}`;
-const API_BASE_URL = `${process.env.REACT_APP_API_URL}` || "http://localhost:8080";
+const API_BASE_URL = `http://3.88.188.127:3000/`;
+// const API_BASE_URL = `${process.env.REACT_APP_API_URL}` || "http://localhost:8080";
 export default API_BASE_URL;
