d9458113009bc76d986bcaa779bb83ce5548d5c9
joelpangop <joelpangopgmail.com>
2025-05-30 16:09:45 -0400

    adapt backend url 31

---
+++ infra/aws-terraform/.secrets/variables.tf
@@ -0,0 +1,23 @@
+variable "instance_type" {
+  description = "set aws instance type"
+  type        = string
+  default     = "t2.nano"
+}
+
+variable "aws_common_tags" {
+  description = "set common tags for all aws resources"
+  type        = map(any)
+  default     = {
+    Name        = "ec2-joeltraining"
+  }
+}
+
+variable "key_name" {
+  description = "Nom de la clé SSH (déjà créée sur AWS)"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Chemin local vers la clé privée .pem"
+  type        = string
+}
